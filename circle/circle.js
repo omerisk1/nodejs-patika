4308cb0e5473665f70dc9d897c56b70970c29600
Ömer IŞIK <isikomer373@gmail.com>
2022-06-14 22:45:47 +0300

    first commit

---
+++ circle/circle.js
@@ -0,0 +1,12 @@
+
+function circleArea(x) {
+  console.log(Math.PI * x * x)
+}
+function circleCircumference(x) {
+  console.log(2 * Math.PI * x)
+}
+
+module.exports = {
+  circleArea,
+  circleCircumference,
+}
---
+++ circle/index.js
@@ -0,0 +1,4 @@
+const circle = require('./circle.js');
+
+circle.circleArea(5);
+circle.circleCircumference(5);
